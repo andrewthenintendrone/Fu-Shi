3276bf7748af63189a5823e594ea78abc630d0c7
Darklight437 <darklight437@gmail.com>
2018-07-02 15:04:09 +1000

    added bamboo models & utils

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils
+{
+    
+
+    //the position to reset to (updated via function)
+    public static Vector3 resetPos;
+    
+
+    // Use this for initialization
+    public static void Init ()
+    {
+        resetPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+	}
+	
+
+    public static void resetPlayer()
+    {
+        GameObject.FindGameObjectWithTag("Player").transform.position = resetPos;
+    }
+
+    public static void updateCheckpoint(Vector3 position)
+    {
+        resetPos = position;
+    }
+}
