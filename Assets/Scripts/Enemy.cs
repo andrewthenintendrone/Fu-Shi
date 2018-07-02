fc98ef10774ee2801e5bc7d3155ac6b635e40e91
Darklight437 <darklight437@gmail.com>
2018-07-03 09:59:39 +1000

    japaneese gobln mask enemy temp in progress

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [Tooltip("number of discrete hits it takes to kill the enemy")]
+    public int health;
+
+    public Vector3[] patrolPoints = new Vector3[2];
+
+    [Tooltip("amount of damage contacting this enemy will do")]
+    public int damageAmt;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
