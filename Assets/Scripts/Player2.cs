86baab09f8adecc7288f1565d19ec8b138328a08
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2018-08-13 16:50:01 +1000

    preliminary character movement
    - it finally resembles an actual game

--- Assets/Scripts/Player2.cs
+++ Assets/Scripts/Player2.cs
@@ -26,11 +26,19 @@ public class Player2 : MonoBehaviour
     // x axis movement speed
     public float moveSpeed;
 
+    // jump force
+    public float jumpForce;
+
+    // gravity multiplier
+    public float gravityMultiplier;
+
     // lasyer mask to interact with
     public LayerMask layerMask;
 
+    public float minDistanceToFloor;
+
     // player velocity
-    private Vector3 velocity;
+    public Vector3 velocity;
 
     #endregion
 
@@ -43,97 +51,119 @@ public class Player2 : MonoBehaviour
 
     private void FixedUpdate()
     {
-        checkGrounded();
-        checkSlope();
+        checkGround();
 
         float xAxis = Input.GetAxis("Horizontal");
 
+        if(xAxis > 0)
+        {
+            checkRight();
+        }
+        if(xAxis < 0)
+        {
+            checkLeft();
+        }
+
+        float jumpAxis = (int)Input.GetAxisRaw("Fire1");
+
         velocity.x = xAxis * moveSpeed;
 
-        if(!grounded)
+        if (!grounded)
         {
-            velocity.y += gravityConstant * Time.fixedDeltaTime;
+            velocity.y += gravityConstant * gravityMultiplier * Time.fixedDeltaTime;
         }
         else
         {
             velocity.y = 0;
+
+            transform.position += Vector3.up * (col.bounds.extents.y - minDistanceToFloor);
+
+            if (jumpAxis == 1)
+            {
+                velocity.y = jumpForce;
+            }
         }
 
-        changeColor(Color.red);
-
-        //if(aboveSlope)
-        //{
-        //    changeColor(Color.red);
-        //}
-        //else
-        //{
-        //    changeColor(Color.white);
-        //}
+        if (aboveSlope)
+        {
+            changeColor(Color.red);
+        }
+        else
+        {
+            changeColor(Color.white);
+        }
 
         transform.position += velocity * Time.fixedDeltaTime;
     }
 
-    // updates whether the player is grounded
-    private void checkGrounded()
+    // moves the player out of wall geometry to the left
+    private void checkLeft()
     {
-        RaycastHit2D hitInfo;
+        RaycastHit2D rayLeft = Physics2D.Raycast(col.bounds.center, Vector2.left, layerMask);
 
-        hitInfo = (Physics2D.Raycast(col.bounds.center, Vector2.down, layerMask));
-
-        // ray hit something
-        if(hitInfo.collider != null)
+        if(rayLeft.collider != null)
         {
-            if(hitInfo.distance < col.bounds.extents.y)
+            if (rayLeft.distance < col.bounds.extents.x)
             {
-                grounded = true;
-                transform.position += Vector3.up * (hitInfo.distance - hitInfo.distance);
-                return;
+                transform.position += Vector3.right * (col.bounds.extents.x - rayLeft.distance);
             }
         }
-
-        grounded = false;
     }
 
-    private void checkSlope()
+    // moves the player out of wall geometry to the right
+    private void checkRight()
     {
+        RaycastHit2D rayRight = Physics2D.Raycast(col.bounds.center, Vector2.right, layerMask);
+
+        if(rayRight.collider != null)
+        {
+            if (rayRight.distance < col.bounds.extents.x)
+            {
+                transform.position += Vector3.left * (col.bounds.extents.x - rayRight.distance);
+            }
+        }
+    }
+
+    // updates whether the player is grounded and on a slope
+    private void checkGround()
+    {
+        minDistanceToFloor = col.bounds.extents.y + 1;
+
         RaycastHit2D hitInfo;
-        RaycastHit2D leftHitInfo;
-        RaycastHit2D rightHitInfo;
+        RaycastHit2D hitInfoLeft;
+        RaycastHit2D hitInfoRight;
 
-        hitInfo = Physics2D.Raycast(col.bounds.center, Vector2.down, layerMask);
-        leftHitInfo = Physics2D.Raycast(new Vector2(col.bounds.min.x, col.bounds.center.y), Vector2.down, layerMask);
-        rightHitInfo = Physics2D.Raycast(new Vector2(col.bounds.max.x, col.bounds.center.y), Vector2.down, layerMask);
+        hitInfo = (Physics2D.Raycast(col.bounds.center, Vector2.down, layerMask));
+        hitInfoLeft = (Physics2D.Raycast(new Vector2(col.bounds.min.x, col.bounds.center.y), Vector2.down, layerMask));
+        hitInfoRight = (Physics2D.Raycast(new Vector2(col.bounds.max.x, col.bounds.center.y), Vector2.down, layerMask));
 
-        float hitDistanceLeft = 0;
-        float hitDistanceRight = 0;
-
-        // we are on a slope in the following conditions
-        // the hit distances of the two raycasts are different
-        // the normal direction of at least one slope is not straight up
-        if(leftHitInfo.collider != null)
+        // ray hit something
+        if (hitInfo.collider != null)
         {
-            hitDistanceLeft = leftHitInfo.distance;
+            //Debug.DrawLine(col.bounds.center, hitInfo.point, Color.green);
+            minDistanceToFloor = Mathf.Min(minDistanceToFloor, hitInfo.distance);
         }
-        if(rightHitInfo.collider != null)
+        if (hitInfoLeft.collider != null)
         {
-            hitDistanceRight = rightHitInfo.distance;
+            //Debug.DrawLine(new Vector3(col.bounds.min.x, col.bounds.center.y, 0), hitInfoLeft.point, Color.red);
+            minDistanceToFloor = Mathf.Min(minDistanceToFloor, hitInfoLeft.distance);
         }
-
-        if(hitDistanceLeft != hitDistanceRight)
+        if (hitInfoRight.collider != null)
         {
-            if(Vector3.Angle(leftHitInfo.normal, Vector3.up) > 5)
-            {
-                aboveSlope = true;
-                return;
-            }
-            if(Vector3.Angle(rightHitInfo.normal, Vector3.up) > 5)
-            {
-                aboveSlope = true;
-                return;
-            }
+            //Debug.DrawLine(new Vector3(col.bounds.max.x, col.bounds.center.y, 0), hitInfoRight.point, Color.blue);
+            minDistanceToFloor = Mathf.Min(minDistanceToFloor, hitInfoRight.distance);
         }
 
-        aboveSlope = false;
+        Debug.DrawLine(col.bounds.center, col.bounds.center + Vector3.down * minDistanceToFloor, Color.yellow);
+
+        grounded = minDistanceToFloor <= col.bounds.extents.y;
+
+        bool slopeLeft = Mathf.Abs(Vector3.Angle(hitInfoLeft.normal, Vector3.up)) >= 5;
+        bool slopeRight = Mathf.Abs(Vector3.Angle(hitInfoRight.normal, Vector3.up)) >= 5;
+
+        aboveSlope = (slopeLeft && slopeRight) ||
+                       (slopeLeft && !slopeRight && hitInfoLeft.point.y >= hitInfoRight.point.y) ||
+                       (!slopeLeft && slopeRight && hitInfoRight.point.y >= hitInfoLeft.point.y);
     }
 
     void changeColor(Color color)
