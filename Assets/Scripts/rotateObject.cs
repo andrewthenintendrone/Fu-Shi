a726e73e9d0eb54d4eaa2b2e7b44bd87c52c2c1d
Darklight437 <darklight437@gmail.com>
2018-08-17 14:34:54 +1000

    rotation scrip implemented

--- Assets/Scripts/rotateObject.cs
+++ Assets/Scripts/rotateObject.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class rotateObject : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    [Tooltip("Rotation rate of an object per physics step,negative numbers will inverse direction")]
+    public float rotationRate = 1;
+
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate ()
+    {
+        transform.Rotate(0, 0, rotationRate);
 	}
 }
