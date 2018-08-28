c5b294d59e783df44851dbc694963476c02777d8
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2018-08-28 11:26:26 +1000

    fixed input bug
    - down press is far more lenient

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -69,6 +69,11 @@ public class Player : MonoBehaviour
 
     void FixedUpdate ()
     {
+        if(character.collisionState.left)
+        {
+            Debug.Break();
+        }
+
         #region get inputs
 
         xAxis = Input.GetAxis("Horizontal");
