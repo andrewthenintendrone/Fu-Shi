9d2fecfdddc70db82ccae546ec9cf9b18c726a26
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2018-09-11 12:00:14 +1000

    comments
    - added more comments

--- Assets/Scripts/DualForwardFocusCamera.cs
+++ Assets/Scripts/DualForwardFocusCamera.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class DualForwardFocusCamera : MonoBehaviour
 {
-
+    // camera component
     [System.NonSerialized]
     [HideInInspector]
     public Camera cam;
 
-
+    // collider to track to
     private Collider2D targetCollider;
 
     [Tooltip("width of the inner box for camera motion")]
@@ -22,15 +22,15 @@ public class DualForwardFocusCamera : MonoBehaviour
     [SerializeField]
     public float height = 3f;
 
-    
+    [Tooltip("width of the detector / outer lines for vertical switching")]
+    [Range(0f, 20f)]
+    public float XThresholdExtents = 0.5f;
 
     [Tooltip("width of the detector / outer lines for vertical switching")]
     [Range(0f, 20f)]
-    public float dualForwardFocusThresholdExtents = 0.5f;
-    [Tooltip("width of the detector / outer lines for vertical switching")]
-    [Range(0f, 20f)]
-    public float dualVerticalFocusThresholdExtents = 0.5f;
+    public float YThresholdExtents = 0.5f;
 
+    // which sides are being focused on on the x and y axis
     private RectTransform.Edge XSideFocus = RectTransform.Edge.Left;
     private RectTransform.Edge YSideFocus = RectTransform.Edge.Bottom;
 
@@ -40,69 +40,62 @@ public class DualForwardFocusCamera : MonoBehaviour
 
     // current velocity
     private Vector3 velocity;
-
-
-
     
     #region MonoBehaviour
 
     void Awake()
     {
-
+        // store component references
         cam = GetComponent<Camera>();
         targetCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<BoxCollider2D>();
     }
 
-
     void FixedUpdate()
     {
         Bounds targetBounds = targetCollider.bounds;
-
-
         Vector3 desiredOffset = Vector3.zero;
-
         Vector3 basePosition = getNormalizedCameraPosition();
 
-
+        // offset from the bounds
         Vector3 deltaPositionFromBounds = Vector3.zero;
+
+        // did the x or y edge focus change this frame
         bool didLastXEdgeContactChange = false;
         bool didLastYEdgeContactChange = false;
 
-        //worldspace position of rect edges
+        // worldspace position of rect edges
         float leftEdge, rightEdge, topEdge, bottomEdge;
 
-        //if the camera focus is a certain side generate the two bound lines on X 
+        // calculate the positions of the relevent edges on the x axis
         if (XSideFocus == RectTransform.Edge.Left)
         {
             rightEdge = basePosition.x - width * 0.5f;
-            leftEdge = rightEdge - dualForwardFocusThresholdExtents * 0.5f;
+            leftEdge = rightEdge - XThresholdExtents * 0.5f;
         }
         else
         {
             leftEdge = basePosition.x + width * 0.5f;
-            rightEdge = leftEdge + dualForwardFocusThresholdExtents * 0.5f;
+            rightEdge = leftEdge + XThresholdExtents * 0.5f;
         }
 
-        //as above but generating the Y component
+        // calculate the positions of the relevent edges on the y axis
         if (YSideFocus == RectTransform.Edge.Top)
         {
             bottomEdge = basePosition.y + height * 0.5f;
-            topEdge = bottomEdge + dualVerticalFocusThresholdExtents * 0.5f;
+            topEdge = bottomEdge + YThresholdExtents * 0.5f;
         }
         else
         {
             topEdge = basePosition.y - height * 0.5f;
-            bottomEdge = topEdge - dualVerticalFocusThresholdExtents * 0.5f;
+            bottomEdge = topEdge - YThresholdExtents * 0.5f;
         }
 
-        
-        //if the player bounds is beyond the outside edge
+        // the player has left the focus area on the x axis
         if (targetBounds.center.x < leftEdge)
         {
-            //how far the player has passed outside the bounds
             deltaPositionFromBounds.x = targetBounds.center.x - leftEdge;
-            //if the player is on the left side of the screen
-            //swap the focus
+
+            // change focus edge
             if (XSideFocus == RectTransform.Edge.Left)
             {
                 didLastXEdgeContactChange = true;
@@ -113,6 +106,7 @@ public class DualForwardFocusCamera : MonoBehaviour
         {
             deltaPositionFromBounds.x = targetBounds.center.x - rightEdge;
 
+            // change focus edge
             if (XSideFocus == RectTransform.Edge.Right)
             {
                 didLastXEdgeContactChange = true;
@@ -120,13 +114,12 @@ public class DualForwardFocusCamera : MonoBehaviour
             }
         }
 
-        //if the player has gone beyond a lower edge
+        // // the player has left the focus area on the y axis
         if (targetBounds.center.y < bottomEdge)
         {
-            //how far the player has passed outside the bounds
             deltaPositionFromBounds.y = targetBounds.center.y - bottomEdge;
-            //if the player is on the bottom side of the screen
-            //swap the focus
+
+            // change focus edge
             if (YSideFocus == RectTransform.Edge.Bottom)
             {
                 didLastYEdgeContactChange = true;
@@ -137,6 +130,7 @@ public class DualForwardFocusCamera : MonoBehaviour
         {
             deltaPositionFromBounds.y = targetBounds.center.y - topEdge;
 
+            // change focus edge
             if (YSideFocus == RectTransform.Edge.Top)
             {
                 didLastYEdgeContactChange = true;
@@ -144,13 +138,14 @@ public class DualForwardFocusCamera : MonoBehaviour
             }
         }
 
+        // calculate the desired position
         float desiredX = (XSideFocus == RectTransform.Edge.Left ? rightEdge : leftEdge);
         desiredOffset.x = targetBounds.center.x - desiredX;
 
         float desiredY = (YSideFocus == RectTransform.Edge.Top ? bottomEdge : topEdge);
         desiredOffset.y = targetBounds.center.y - desiredY;
 
-        // if we didnt switch direction this works much like a normal camera window
+        // if we didnt change focus this frame update the desired offset
         if (!didLastXEdgeContactChange)
         {
             desiredOffset.x = deltaPositionFromBounds.x;
@@ -161,21 +156,23 @@ public class DualForwardFocusCamera : MonoBehaviour
             desiredOffset.y = deltaPositionFromBounds.y;
         }
 
+        // update the target position
         Vector3 targetPosition = transform.position + desiredOffset;
 
-
+        // smooth the movement to the target position
         targetPosition = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
 
+        // finally set position
         transform.position = targetPosition;
-
     }
 
 #if UNITY_EDITOR
+
     void OnDrawGizmos()
     {
+        // calculate positions
         Vector3 positionInFrontOfCamera = getNormalizedCameraPosition();
         positionInFrontOfCamera.z = transform.position.z - 1;
-
         
         Gizmos.color = new Color(0f, 0.5f, 0.6f);
 
@@ -190,17 +187,15 @@ public class DualForwardFocusCamera : MonoBehaviour
         Gizmos.DrawLine(bounds.min + Vector3.up * bounds.size.y, bounds.max);
         Gizmos.DrawLine(bounds.max + Vector3.down * bounds.size.y, bounds.min);
 
-
         //draw outer detection bounds
-        bounds.Expand(new Vector3(dualForwardFocusThresholdExtents, dualVerticalFocusThresholdExtents));
+        bounds.Expand(new Vector3(XThresholdExtents, YThresholdExtents));
         Gizmos.color = Color.blue;
         Gizmos.DrawLine(bounds.min, bounds.min + Vector3.up * bounds.size.y);
         Gizmos.DrawLine(bounds.max, bounds.max + Vector3.down * bounds.size.y);
         Gizmos.DrawLine(bounds.min + Vector3.up * bounds.size.y, bounds.max);
         Gizmos.DrawLine(bounds.max + Vector3.down * bounds.size.y, bounds.min);
-
-
     }
+
 #endif
 
     #endregion
