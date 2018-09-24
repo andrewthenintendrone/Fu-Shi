778d472392cdc0b5832396e1675bf427522b5e24
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2018-09-24 10:05:10 +1000

    quick main menu
    - doesn't work quite yet need to load or create new game on the fly

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // start new game
+    public void newGame()
+    {
+        Debug.Log("new game");
+        SceneManager.LoadScene(1);
+    }
+
+    public void loadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void exitToDesktop()
+    {
+        Application.Quit();
+    }
+}
