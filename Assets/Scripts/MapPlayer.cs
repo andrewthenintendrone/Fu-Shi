29af7c8a5f59fc8d4bed0d500db399473151811a
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2018-10-09 14:59:13 +1100

    more progress on map
    - nailed down how to do the map in a good way
    - programmer art

--- Assets/Scripts/MapPlayer.cs
+++ Assets/Scripts/MapPlayer.cs
@@ -2,25 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class MapPlayer : MonoBehaviour
 {
-    // actual position of the player gameobject
-    private Transform actualPlayerPosition;
+    [SerializeField]
+    [Tooltip("map camera position for this room.")]
+    private Vector2 cameraPosition;
 
-    // where does the player start
-    public Vector3 playerStartPosition;
+    [SerializeField]
+    [Tooltip("map camera orthographic scale for this room. (zoom)")]
+    private float cameraOrthoScale;
 
-    public float scale;
-
-    private void Awake()
+    private void OnTriggerEnter2D(Collider2D collider)
     {
-        actualPlayerPosition = GameObject.FindGameObjectWithTag("Player").transform;
+        if(collider.tag == "Player")
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+            GameObject.Find("MapCam").transform.position = new Vector3(transform.position.x + cameraPosition.x, transform.position.y + cameraPosition.y, -10);
+            GameObject.Find("MapCam").GetComponent<Camera>().orthographicSize = Mathf.Floor(cameraOrthoScale);
+        }
     }
 
-    private void OnEnable()
+    private void OnTriggerExit2D(Collider2D collider)
     {
-        Vector3 actualPlayerOffset = actualPlayerPosition.position - playerStartPosition;
-
-        GetComponent<RectTransform>().localPosition = Vector3.right * 10;
+        if (collider.tag == "Player")
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
     }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(new Vector3(transform.position.x + cameraPosition.x, transform.position.y + cameraPosition.y, -10), new Vector3(cameraOrthoScale, cameraOrthoScale, 0.1f));
+    }
+
+#endif
 }
