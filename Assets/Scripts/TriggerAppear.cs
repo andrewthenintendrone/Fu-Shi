b1324133a354fae29a842da7dc36d4244882bf52
Tater-totz <Michaelt1998.mt@gmail.com>
2018-10-16 15:57:11 +1100

    Merge branch 'master' of https://github.com/andrewthenintendrone/Fu-Shi

---
+++ Assets/Scripts/TriggerAppear.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAppear : MonoBehaviour
+{
+    public GameObject Text;
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == ("Player"))
+        {
+            Text.SetActive(true);
+
+
+        }
+    }
+}
