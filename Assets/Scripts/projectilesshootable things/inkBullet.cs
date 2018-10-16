6131e81f2ad796822ca20cb2a1856b5839a133c4
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2018-10-16 14:50:32 +1100

    finished bad map system
    - good version will come later

--- Assets/Scripts/projectilesshootable things/inkBullet.cs
+++ Assets/Scripts/projectilesshootable things/inkBullet.cs
@@ -38,7 +38,7 @@ public class inkBullet: MonoBehaviour
         }
 
         if (collision.gameObject.GetComponent<enemyProjectile>() == null && collision.gameObject.GetComponent<inkBullet>() == null && collision.gameObject.GetComponent<Player>() == null
-            && collision.gameObject.GetComponent<Inkmeleeslash>() == null && GetComponent<MapPlayer>() == null)
+            && collision.gameObject.GetComponent<Inkmeleeslash>() == null)
         {
         
          Destroy(gameObject);
