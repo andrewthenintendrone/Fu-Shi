d507b044e6e79a2db4c7408f52fec4a80ffbc16b
Darklight437 <darklight437@gmail.com>
2018-10-17 13:00:40 +1100

    Believable platform WIP

---
+++ Assets/Non_model Animations/AnimationOnLanding.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationOnLanding : MonoBehaviour
+{
+
+    [SerializeField]
+    private Animator animator;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       //Debug.Log("stepped");
+        animator.SetTrigger("JumpedOn");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        animator.SetTrigger("JumpedOn");
+    }
+}
