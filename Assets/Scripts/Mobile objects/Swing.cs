9846eaaca9c30eab28d13931a5ee7110752b21ab
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2018-10-23 16:35:51 +1100

    quality of life
    - triggerAppear now shows radii for the designers
    - triggerAppear no longer takes into account the z axis
    - swing has a nice gizmo to show how it will swing

--- Assets/Scripts/Mobile objects/Swing.cs
+++ Assets/Scripts/Mobile objects/Swing.cs
@@ -1,9 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 public class Swing : MonoBehaviour
 {
@@ -24,22 +21,19 @@ public class Swing : MonoBehaviour
         transform.eulerAngles = Vector3.forward * Mathf.Sin(Time.fixedTime * swingSpeed) * maxSwingAngle * (flip ? -1 : 1);
 	}
 
-#if UNITY_EDITOR
-
-    private void OnDrawGizmos()
+    private void OnDrawGizmosSelected()
     {
-        Gizmos.color = Color.green;
+        Gizmos.color = Color.cyan;
 
-        Vector3 linePos = Vector3.zero;
-        Vector3 prevLinePos = Vector3.zero;
+        Vector3 linePos = transform.position;
+        Vector3 prevLinePos = transform.position;
 
-        for (float i = -maxSwingAngle; i < maxSwingAngle; i++)
+        for (float i = -32; i < 33; i++)
         {
             prevLinePos = linePos;
-            linePos = new Vector3(Mathf.Sin(i), Mathf.Cos(i), 0) * 100.0f;
+            linePos = transform.position - new Vector3(Mathf.Sin(Mathf.Deg2Rad * i / 32 * maxSwingAngle), Mathf.Cos(Mathf.Deg2Rad * i / 32 * maxSwingAngle), 0) * 32.0f;
             Gizmos.DrawLine(prevLinePos, linePos);
         }
+        Gizmos.DrawLine(linePos, transform.position);
     }
-
-#endif
 }
--- Assets/Scripts/TriggerAppear.cs
+++ Assets/Scripts/TriggerAppear.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class TriggerAppear : MonoBehaviour
 {
@@ -22,10 +25,23 @@ public class TriggerAppear : MonoBehaviour
 
     private void Update()
     {
-        float distToPlayer = Vector3.Magnitude(Utils.getPlayer().transform.position - transform.position);
+        float distToPlayer = Vector2.Distance(Utils.getPlayer().transform.position, transform.position);
 
         color.a = 1 - Mathf.Clamp01((distToPlayer - opaqueDistance) / (transparentDistance - opaqueDistance));
 
         GetComponent<Renderer>().material.color = color;
     }
+
+#if UNITY_EDITOR
+
+    void OnDrawGizmosSelected()
+    {
+        Handles.color = Color.red;
+        Handles.DrawWireDisc(transform.position, -Vector3.forward, transparentDistance);
+
+        Handles.color = Color.yellow;
+        Handles.DrawWireDisc(transform.position, -Vector3.forward, opaqueDistance);
+    }
+
+#endif
 }
