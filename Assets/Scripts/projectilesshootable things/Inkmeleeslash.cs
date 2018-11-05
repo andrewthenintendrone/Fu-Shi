0325a0b3365f7766c608dae3715db14d916e0b2a
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2018-11-05 15:10:55 +1100

    ink melee slash now inks leaves again

--- Assets/Scripts/projectilesshootable things/Inkmeleeslash.cs
+++ Assets/Scripts/projectilesshootable things/Inkmeleeslash.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inkmeleeslash : MonoBehaviour {
-
+public class Inkmeleeslash : MonoBehaviour
+{
     [SerializeField]
     private GameObject inkbulletprefab;
 
@@ -28,20 +28,19 @@ public class Inkmeleeslash : MonoBehaviour {
         Destroy(gameObject, lifetime);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        // check if object has inkable surface
-        if (collision.gameObject.GetComponentInChildren<inkableSurface>() != null)
-        {
-            collision.gameObject.GetComponentInChildren<inkableSurface>().Inked = true;
-        }
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<Enemy>() != null)
         {
             collision.gameObject.GetComponent<Enemy>().health--;
+        }
+        else
+        {
+            // check if object has inkable surface
+            if (collision.gameObject.GetComponentInChildren<inkableSurface>() != null)
+            {
+                collision.gameObject.GetComponentInChildren<inkableSurface>().Inked = true;
+            }
         }
     }
 }
