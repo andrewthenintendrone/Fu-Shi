35d6623c106004f6073ac789cad8c2be318f1d4c
Darklight437 <darklight437@gmail.com>
2018-11-19 11:05:41 +1100

    enemies should spawn particles now

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -63,7 +63,7 @@ public class Enemy : MonoBehaviour
                 {
                     gameObject.GetComponent<patrolmove>().enabled = false;
                 }
-                Instantiate(deathParticles, transform.position, Quaternion.identity, transform);
+                Instantiate(deathParticles, transform.position, Quaternion.identity);
                 SoundManager.instance.playDeathFX();
                 
                 this.enabled = false;
